2071c009023f7758fcd7fa541a3a80bedab698e5
unknown <joshlin0927@gmail.com>
2021-09-28 11:24:20 +0800

    lesson2 sec hr

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,10 @@
 import { useState } from 'react'
+import Logo from './logo.svg'
+import { debug, devUrl, prodUrl } from './config'
 import MyButton from './components/MyButton'
+import './App.css'
+
+const imgUrl = debug ? devUrl : prodUrl
 
 function App() {
   const [total, setTotal] = useState(0)
@@ -9,18 +14,63 @@ function App() {
 
   return (
     <>
-    <h1>{total}</h1>
-    <button onClick={() => {setTotal(total + 1)}}>+</button>
-    <button onClick={() => {setTotal(total - 1)}}>-</button>
-    <br/>
-    <img src="https://www.collinsdictionary.com/images/full/book_181404689_1000.jpg" width="100px"/>
-    <br />
-    <MyButton />
+      {1}
+      <hr />
+      {'abc'}
+      {/* 可以正常呈現 */}
+      <hr />
+      {true}
+      <hr />
+      {false}
+      <hr />
+      {null}
+      <hr />
+      {undefined}
+      <hr />
+      {/* 上面四個都不會顯示 */}
+
+      {[1, 2, 3, 4]}
+      {/* 呈現方式會像是[1, 2, 3, 4].join()，只留下字串 1234 */}
+      <hr />
+      {/* {{ a: 1, b: 2 }} */}
+      {/* 純物件會讓程式產生錯誤，需要調整 */}
+      <hr />
+      {() => {
+        console.log(1)
+      }}
+      {/* 也不會顯示 */}
+      <hr />
+      <h1>{total}</h1>
+      <button
+        onClick={() => {
+          setTotal(total + 1)
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setTotal(total - 1)
+        }}
+      >
+        -
+      </button>
+      <br />
+      <img
+        src="https://www.collinsdictionary.com/images/full/book_181404689_1000.jpg"
+        width="100px"
+      />
+      <br />
+
+       {/* 放在public資料夾，要用http://XXXX/ */}
+       <img src={`${imgUrl}/images/17.jpg`} alt="" />
+      {/* --------------------- */}
+      {/* 放在src資料夾，要用import(第2行) */}
+      <img src={Logo} alt="" />
+
+      <MyButton />
     </>
   )
 }
 
-
 export default App
-
-
---
+++ my-app/src/config.js
@@ -0,0 +1,3 @@
+export const debug = true
+export const devUrl = 'http://localhost:3003'
+export const prodUrl = 'http://www.abc.com'
